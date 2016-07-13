1225fa2bfb41bd32ae4677e53952fd80428559d2
mrmojica <mrmojica510@gmail.com>
2016-07-13 09:03:00 -0700

    added remove button/cleaned up html

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
 
+/************Add Button*************/
 
 $('#addButton').click(function() {
 	var $items = $('#newItem').val();
-	$('.listItems').append('<li class="item"><input class="check" type="checkbox">' + $items + '<button class="edit" type="button"><i class="fa fa-pencil" aria-hidden="true"></i></button><button class="remove" type="button"><i class="fa fa-trash" aria-hidden="true"></i></button></li>');
-
+	$('.listItems').append('<li class="item"><input class="check" type="checkbox">' + '<strong>' +$items + '</strong>' + '<button class="edit" type="button"><i class="fa fa-pencil" aria-hidden="true"></i></button><button class="remove" type="button"><i class="fa fa-trash" aria-hidden="true"></i></button></li>');
 });
 
-/* selected document so function will apply to new added items on click with .check */
-$(document).on('click', '.check', function() {
+/************Checked Button*************/
+
+/* selected .listItems so function will apply to new added items on click with .check */
+$('.listItems').on('click', '.check', function() {
 	if ($(this).is(':checked')) {
 	/*	console.log('Checked'); */
 		$(this).parent('.item').addClass('completed');
@@ -16,10 +18,23 @@ $(document).on('click', '.check', function() {
     /*console.log('Unchecked'); */
     	$(this).parent('.item').removeClass('completed');
     }
+});
+
+/************Edit Button*************/
+
+$('.edit').click(function(){
+	var currentInput = $(this).parent();
+	
 
 
 });
 
+
+/************Remove Button*************/
+
+$('.listItems').on('click', '.remove', function() {
+	$(this).parent('li').remove();
+});
 
 
 
